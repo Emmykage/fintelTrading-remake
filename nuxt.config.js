79563f3116bd3fb6681aaac52760981a9021de5f
morris menanya <emmiemenz@gmail.com>
2024-04-03 19:05:50 +0100

    feature: add bootstrap

--- nuxt.config.js
+++ nuxt.config.js
@@ -47,11 +47,12 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
+  css: ['@/assets/css/main.css'
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
+    '~/plugins/bootstrap-vue.client'
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
