21bc37d0d6331323c33442c2fcae2d293e1bfe15
morris menanya <emmiemenz@gmail.com>
2024-04-19 14:44:10 +0100

    add widget id

--- plugins/tawk-messenger.client.js
+++ plugins/tawk-messenger.client.js
@@ -4,6 +4,6 @@ import TawkMessengerVue from '@tawk.to/tawk-messenger-vue-2'
 export default function () {
   Vue.use(TawkMessengerVue, {
     propertyId: '66226624a0c6737bd12e1a8d',
-    widgetId: '1hpnmnisc'
+    widgetId: '1hrr5u0ip'
   })
 }
