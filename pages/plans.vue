278483cd7e8931652eeab4ca7004c4f87afd03dc
morris menanya <emmiemenz@gmail.com>
2024-04-25 15:34:22 +0100

    fix white card background

--- pages/plans.vue
+++ pages/plans.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="dark:bg-[#003b6d]">
     <section class="text-gray-600 body-font overflow-hidden">
-      <div class="container px-5 py-24 mx-auto">
+      <div class="container px-5 py-14 mx-auto">
         <div class="flex flex-col text-center w-full mb-20">
           <h1 class="sm:text-4xl text-3xl font-medium title-font mb-2 text-white">Investment Plans</h1>
           <p class="lg:w-2/3 mx-auto leading-relaxed text-base text-gray-500"> Outlined are our plans.</p>
@@ -10,28 +10,145 @@
             <button class="py-1 px-4 focus:outline-none">Annually</button>
           </div>
         </div>
-        <div class="flex justify-center flex-wrap -m-4">
-          <div class="p-4 xl:w-1/4 md:w-1/2 w-full text-white">
-            <div class="h-full p-6 rounded-lg border-2 border-gray-300 flex flex-col relative overflow-hidden">
-              <h2 class="text-sm tracking-widest title-font mb-1 font-medium">Platinum Plan </h2>
-              <!-- <h1 class="text-5xl text-gray-900 pb-4 mb-4 border-b border-gray-200 leading-none">$400 to $4999</h1> -->
-              <h1 class="text-5xl text-gray-900 leading-none flex items-center pb-4 mb-4 border-b border-gray-200">
-                <span>$400</span>
-                <span class="text-lg ml-1 font-normal text-gray-500">/mo</span>
-              </h1>
-              <p class="flex items-center text-sm text-gray-800 font-semibold mb-2">
+        <div class="flex items-start justify-center flex-wrap -m-4 gap-5">
+          <div class="xl:w-1/4 md:w-1/2 w-full text-white bg-white">
+            <div class="h-full p-0 rounded-lg shadow flex flex-col relative overflow-hidden">
+              <div class="h-64 bg-gray-100 text-center py-5">
+                <h2 class="text-lg text-center tracking-widest title-font mb-1 font-semibold text-gray-800">Platinum Plan </h2>
+                <!-- <h1 class="text-5xl text-gray-900 pb-4 mb-4 border-b border-gray-200 leading-none">$400 to $4999</h1> -->
+                <h1 class="text-5xl text-gray-900 leading-none">
+                  <p class="block font-semibold">$400</p>
+                  <p class="text-lg ml-1 font-medium text-gray-500">/month</p>
+                </h1>
+
+                <nuxt-link to="/dashboard" class="block w-max m-auto bg-gray-950 text-white py-2 px-10 rounded-sm my-4"> Get Started</nuxt-link>
+              </div>
+
+              <div class="py-4">
+
+
+              <p class="flex px-6 items-center text-sm text-gray-800 font-semibold mb-2">
                 <span class="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
                   <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" class="w-3 h-3" viewBox="0 0 24 24">
                     <path d="M20 6L9 17l-5-5"></path>
                   </svg>
                 </span>  The Platinum Plan (29%) Monthly
               </p>
-              <p class="flex text-sm text-gray-800 mb-2">
+
+              <p class="px-6 pt-2 font-semibold flex text-sm mb-2 text-gray-800">
                 <span class="w-4 h-4 mr-2 mt-1 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
                   <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" class="w-3 h-3" viewBox="0 0 24 24">
                     <path d="M20 6L9 17l-5-5"></path>
                   </svg>
-                </span> The Platinum plan starts trading from $400 to $4999
+                </span> Daily profits return guaranteed
+              </p>
+              <p class="px-6 pt-2 font-semibold flex text-sm text-gray-800 mb-2">
+                <span class="w-4 h-4 mr-2 mt-1 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
+                  <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" class="w-3 h-3" viewBox="0 0 24 24">
+                    <path d="M20 6L9 17l-5-5"></path>
+                  </svg>
+                </span> Annual trading reports
+              </p>
+              <p class="px-6 pt-2 font-semibold flex text-sm text-gray-800 mb-2">
+                <span class="w-4 h-4 mr-2 mt-1 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
+                  <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" class="w-3 h-3" viewBox="0 0 24 24">
+                    <path d="M20 6L9 17l-5-5"></path>
+                  </svg>
+                </span> Eligible for compounding returns (exponential growth)
+
+              </p>
+              <p class="px-6 pt-2 font-semibold flex text-sm text-gray-800 mb-2">
+                <span class="w-4 h-4 mr-2 mt-1 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
+                  <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" class="w-3 h-3" viewBox="0 0 24 24">
+                    <path d="M20 6L9 17l-5-5"></path>
+                  </svg>
+                </span> Weekly/monthly payouts
+              </p>
+
+              <!-- <p class="flex items-center text-gray-600 mb-6">
+                <span class="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
+                  <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" class="w-3 h-3" viewBox="0 0 24 24">
+                    <path d="M20 6L9 17l-5-5"></path>
+                  </svg>
+                </span>Mixtape chillwave tumeric
+              </p> -->
+            </div>
+
+            </div>
+          </div>
+          <div class="xl:w-1/4 md:w-1/2 w-full text-white bg-white">
+            <div class="h-full p-0 rounded-lg shadow flex flex-col relative overflow-hidden">
+              <div class="h-64 bg-gray-100 text-center py-5">
+                <h2 class="text-lg text-center tracking-widest title-font mb-1 font-semibold text-gray-800">Diamond Plan </h2>
+                <!-- <h1 class="text-5xl text-gray-900 pb-4 mb-4 border-b border-gray-200 leading-none">$400 to $4999</h1> -->
+                <h1 class="text-5xl text-gray-900 leading-none">
+                  <p class="block font-semibold">$5000</p>
+                  <p class="text-lg ml-1 font-medium text-gray-500">/month</p>
+                </h1>
+
+                <nuxt-link to="/dashboard" class="block w-max m-auto bg-gray-950 text-white py-2 px-10 rounded-sm my-4"> Get Started</nuxt-link>
+              </div>
+
+              <div class="py-4">
+
+
+              <p class="flex px-6 items-center text-sm text-gray-800 font-semibold mb-2">
+                <span class="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
+                  <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" class="w-3 h-3" viewBox="0 0 24 24">
+                    <path d="M20 6L9 17l-5-5"></path>
+                  </svg>
+                </span>  The Diamond plan starts trading from $5000 to $5M
+              </p>
+              <p class="px-6 pt-2 text-gray-800 font-semibold flex text-sm mb-2">
+                <span class="w-4 h-4 mr-2 mt-1 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
+                  <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" class="w-3 h-3" viewBox="0 0 24 24">
+                    <path d="M20 6L9 17l-5-5"></path>
+                  </svg>
+                </span> The Diamond Plan (35%) Monthly
+              </p>
+              <p class="px-6 pt-2 font-semibold flex text-sm mb-2 text-gray-800">
+                <span class="w-4 h-4 mr-2 mt-1 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
+                  <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" class="w-3 h-3" viewBox="0 0 24 24">
+                    <path d="M20 6L9 17l-5-5"></path>
+                  </svg>
+                </span> Daily profits return guaranteed
+              </p>
+              <p class="px-6 pt-2 font-semibold flex text-sm text-gray-800 mb-2">
+                <span class="w-4 h-4 mr-2 mt-1 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
+                  <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" class="w-3 h-3" viewBox="0 0 24 24">
+                    <path d="M20 6L9 17l-5-5"></path>
+                  </svg>
+                </span> Annual trading reports
+              </p>
+              <p class="px-6 pt-2 font-semibold flex text-sm text-gray-800 mb-2">
+                <span class="w-4 h-4 mr-2 mt-1 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
+                  <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" class="w-3 h-3" viewBox="0 0 24 24">
+                    <path d="M20 6L9 17l-5-5"></path>
+                  </svg>
+                </span> Eligible for compounding returns (exponential growth)
+
+              </p>
+              <p class="px-6 pt-2 font-semibold flex text-sm text-gray-800 mb-2">
+                <span class="w-4 h-4 mr-2 mt-1 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
+                  <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" class="w-3 h-3" viewBox="0 0 24 24">
+                    <path d="M20 6L9 17l-5-5"></path>
+                  </svg>
+                </span> Weekly/monthly payouts
+              </p>
+              <p class="px-6 pt-2 font-semibold flex text-sm text-gray-800 mb-2">
+                <span class="w-4 h-4 mr-2 mt-1 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
+                  <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" class="w-3 h-3" viewBox="0 0 24 24">
+                    <path d="M20 6L9 17l-5-5"></path>
+                  </svg>
+                </span> Lifetime trading
+
+              </p>
+              <p class="px-6 pt-2 font-semibold flex text-sm text-gray-800 mb-2">
+                <span class="w-4 h-4 mr-2 mt-1 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
+                  <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" class="w-3 h-3" viewBox="0 0 24 24">
+                    <path d="M20 6L9 17l-5-5"></path>
+                  </svg>
+                </span> Private fund manager
               </p>
               <!-- <p class="flex items-center text-gray-600 mb-6">
                 <span class="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
@@ -41,31 +158,10 @@
                 </span>Mixtape chillwave tumeric
               </p> -->
             </div>
-          </div>
-          <div class="p-4 xl:w-1/4 md:w-1/2 w-full text-white">
-            <div class="h-full p-6 rounded-lg border-2 border-indigo-500 flex flex-col relative overflow-hidden">
-              <span class="bg-indigo-500 text-white px-3 py-1 tracking-widest text-xs absolute right-0 top-0 rounded-bl">POPULAR</span>
-              <h2 class="text-sm tracking-widest title-font mb-1 font-medium">Diamond Plan</h2>
-              <h1 class="text-5xl text-gray-900 leading-none flex items-center pb-4 mb-4 border-b border-gray-200">
-                <span>$5000</span>
-                <span class="text-lg ml-1 font-normal text-gray-500">/mo</span>
-              </h1>
-              <p class="flex items- text-gray-800 font-semibold text-sm mb-2">
-                <span class="w-4 h-4 mr-2 mt-1 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
-                  <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" class="w-3 h-3" viewBox="0 0 24 24">
-                    <path d="M20 6L9 17l-5-5"></path>
-                  </svg>
-                </span>The Diamond plan starts trading from $5000 to $5M
-              </p>
-              <p class="flex text-gray-800 font-semibold text-sm mb-2">
-                <span class="w-4 h-4 mr-2 mt-1 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
-                  <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" class="w-3 h-3" viewBox="0 0 24 24">
-                    <path d="M20 6L9 17l-5-5"></path>
-                  </svg>
-                </span> The Diamond Plan (35%) Monthly
-              </p>
+
             </div>
           </div>
+
         </div>
       </div>
     </section>
