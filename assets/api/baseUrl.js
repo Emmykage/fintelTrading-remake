7e27a4a1c6a0c0e76a15080a289711be663fc806
morris menanya <emmiemenz@gmail.com>
2024-06-07 15:34:20 +0100

    add usrl

--- assets/api/baseUrl.js
+++ assets/api/baseUrl.js
@@ -1,4 +1,4 @@
-const baseUrl = "http://localhost:3000/api/v1/"
-// const baseUrl = "https://fintelvalues-70aea147fb44.herokuapp.com/api/v1/"
+// const baseUrl = "http://localhost:3000/api/v1/"
+const baseUrl = "https://fintelvalues-70aea147fb44.herokuapp.com/api/v1/"
 
 export {baseUrl}
